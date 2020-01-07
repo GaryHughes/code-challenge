1ed8af310a7fbe44a3285fa0cc02c0fe4b2e981d
Gary Hughes <garyedwardhughes@gmail.com>
2020-01-07 14:50:47 +1100

    Skeleton for XML parser. Copy sample data to build output for test runner.

---
+++ dotnet-code-challenge.Test/XmlFeedTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnet_code_challenge
+{
+    [TestClass]
+    public class XmlFeedTests
+    {
+        [TestMethod]
+        public void TestDeserialise()
+        {
+      
+
+        }
+    }
+}
